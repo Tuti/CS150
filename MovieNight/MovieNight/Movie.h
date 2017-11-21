66577cf11d415aefef829167ea6a4e08a281474a
Jose Huerta <josehuerta115@gmail.com>
2017-11-21 09:16:08 -0800

    Update repo

---
+++ MovieNight/MovieNight/Movie.h
@@ -0,0 +1,39 @@
+#pragma once
+// MovieNight.h : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <string>
+
+using namespace std;
+
+class Movie
+{
+	//Data Declarations
+	string title;
+	string director;
+	int yearReleased;
+	int runningTime;
+
+public:
+	//Constructors 
+	Movie();
+	Movie(const string title, const string director, const int yearReleased, const int runningTime);
+
+	//Getters
+	string getTitle();
+	string getDirector();
+	int getYearReleased();
+	int getRunningTime();
+
+	//Setters
+	void setTitle(const string title);
+	void setDirector(const string director);
+	void setYearReleased(const int yearReleased);
+	void setRunningTime(const int runningTime);
+
+	//Functions
+
+
+
+};
